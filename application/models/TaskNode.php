080a263ffe091441a60e1cdc23a21a2cfb15c663
Bradley Holt <bradley.holt@foundline.com>
2010-06-01 21:53:49 -0400

    Create and test task node.

---
+++ application/models/TaskNode.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Tasked
+ *
+ * @category    Tasked
+ * @package     Tasked_Model
+ * @copyright   Copyright (c) 2005-2010 Found Line, Inc. (http://www.foundline.com/)
+ * @license     http://foundline.com/legal/software-license/ New BSD License
+ */
+
+/**
+ * Task Node
+ *
+ * @category    Tasked
+ * @package     Tasked_Model
+ * @copyright   Copyright (c) 2005-2010 Found Line, Inc. (http://www.foundline.com/)
+ * @license     http://foundline.com/legal/software-license/ New BSD License
+ */
+class Tasked_Model_TaskNode
+{
+    /**
+     * @var Zend_Date
+     */
+    private $_date;
+
+    /**
+     * Construct
+     *
+     * @param Zend_Date $date
+     * @return void
+     */
+    public function __construct(Zend_Date $date = null)
+    {
+        $this->_date = $date;
+    }
+
+    /**
+     * Get Date
+     *
+     * @return Zend_Date
+     */
+    public function getDate()
+    {
+        if (null === $this->_date) {
+            return null;
+        }
+        return clone $this->_date;
+    }
+}
---
+++ tests/application/models/TaskNodeTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Tasked
+ *
+ * @category    Tasked
+ * @package     Tasked_Model
+ * @copyright   Copyright (c) 2005-2010 Found Line, Inc. (http://www.foundline.com/)
+ * @license     http://foundline.com/legal/software-license/ New BSD License
+ */
+
+/**
+ * Task Node Test
+ *
+ * @group       Tasked
+ * @group       Model
+ * @group       TaskNode
+ * @category    Tasked
+ * @package     Tasked_Model
+ * @copyright   Copyright (c) 2005-2010 Found Line, Inc. (http://www.foundline.com/)
+ * @license     http://foundline.com/legal/software-license/ New BSD License
+ */
+class Tasked_Model_TaskNodeTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        include APPLICATION_PATH . '/../tests/application/bootstrap.php';
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testSetAndGetDate()
+    {
+        $value = new Zend_Date();
+        $taskNode = new Tasked_Model_TaskNode($value);
+        $this->assertEquals(
+            $value,
+            $taskNode->getDate()
+        );
+    }
+
+    public function testSetAndGetNullDate()
+    {
+        $taskNode = new Tasked_Model_TaskNode();
+        $this->assertNull(
+            $taskNode->getDate()
+        );
+    }
+}
